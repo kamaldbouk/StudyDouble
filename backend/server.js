cffdb5a7760a26ed72d5cede9daa94853d01e968
kamaldbouk <kamaldbouk13@gmail.com>
2025-01-24 11:24:37 +0200

    signup api ready - tested on postman

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const sessionRoutes = require('./routes/sessions')
+const userRoutes = require('./routes/user')
 
 // express app
 const app = express()
@@ -17,6 +18,7 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/sessions', sessionRoutes)
+app.use('/api/user', userRoutes)
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
